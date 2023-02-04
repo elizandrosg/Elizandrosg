1b099e7c43fdbbfb50a0304c4279214d96a12244
Elizandro Gonçalves <55092252+elizandrosg@users.noreply.github.com>
2023-02-04 17:34:29 -0300

    Add files via upload

---
+++ aula004.py
@@ -0,0 +1,19 @@
+"""
+Tipos de dados
+str - string - textos
+int - inteiro - numero negativo e positivo sem pontos
+float - real/ponto flutuante - 10.50, 2.50, -10.10, -53.37, 0.0
+bool - booleano/logico - true/false 10 == 10
+"""
+print('luiz', type('luiz'))
+print(12, type(12))
+print(10.5, type(10.5))
+print(10==10, type(10==10))
+print('L'=='L', type('L'=='L'))
+# exercicios
+print('Elizandro', type('Elizandro'))
+print(41, type(41))
+print(1.70, type(1.70))
+print(41>18, type(41>18))
+
+
