56a16e167b42e6b4e05ca3513e5d868d030a21c2
Elizandro Gonçalves <55092252+elizandrosg@users.noreply.github.com>
2023-02-04 17:32:25 -0300

    Add files via upload

---
+++ aula001.py
@@ -0,0 +1,15 @@
+"""
+Algum texto
+"""
+print('Comentarios')
+'''
+Comentarios
+'''
+# Comentario
+print('Linha 1')
+print('Linha 2')  # sempre colocar dois espaços, pois trata-se de padrão de programadores.
+# comentarios
+print('Linha 3')
+print('Linha 4')
+print('Linha 5')
+# Final
