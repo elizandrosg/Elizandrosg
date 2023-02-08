3a69ee73a37dc598fa9d201daddbb23b896892c1
Elizandro Gonçalves <55092252+elizandrosg@users.noreply.github.com>
2023-02-08 19:52:37 -0300

    Add files via upload

---
+++ aula016.py
@@ -0,0 +1,28 @@
+"""
+Manipulando Strings
+*String indices
+*Fatiamento de strings [inicio:fim:passo]
+*Função built-in, len, abs, type, print, etc...
+Essas funções podem ser usadas diretamente em cada tipo
+
+Você pode conferir tudo isso em:
+https://docs.python.org/3/library/stdtypes.html (tipos built-in)
+https://docs.python.org/3/library/funcions.html (funções built-in)
+"""
+#positivos [012345678]
+texto =    'python s2'
+#negativos-[987654321]
+
+
+nova_str = texto[0:6:3]
+
+print(nova_str)
+
+"""
+url = "https://docs.python.org/"
+
+print(url[:-1])
+"""
+
+
+#print(texto [3])
