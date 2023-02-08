a6de84a902f8a9f911d3ec934e168d53a787a63c
Elizandro Gonçalves <55092252+elizandrosg@users.noreply.github.com>
2023-02-08 19:49:50 -0300

    Add files via upload

---
+++ aula009.py
@@ -0,0 +1,16 @@
+# condições IF, ELIF e ELSE
+
+nome = 'Elizandro'
+
+if True:
+    input("digite seu nome. ")
+
+    print("verdadeiro.")
+else:
+    print("Falso")
+#elif False:
+#    print('agora é verdadeito')
+#elif True:
+#    print('Mais agora é verdadeiro')
+#else:
+#    print("não é verdadeiro")
