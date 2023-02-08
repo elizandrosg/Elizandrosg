9ed0cd048678780cb36c38f3906adbfcb165d565
Elizandro Gonçalves <55092252+elizandrosg@users.noreply.github.com>
2023-02-08 19:50:59 -0300

    Add files via upload

---
+++ aula012.py
@@ -0,0 +1,15 @@
+#  len quatidade de caracteres dentro da string obs. so funciona com str
+
+#usuario = input('Digite seu usuario: ')
+#qtd_caracteres = len(usuario)
+
+#if qtd_caracteres<6:
+#    print('Voce precisa digitar pelo menos 6 caracteres.')
+
+#print(usuario, qtd_caracteres, type(qtd_caracteres))
+
+string1 = input('Digite seu nome: ')
+string2 = input('Digite seu sobrenome: ')
+
+print(f'A quantidade total de caracteres digitado foi {len(string1) + len(string2)}')
+
