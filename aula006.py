0760e1a6cb580f1de730e706362b3e82c452b76a
Elizandro Gonçalves <55092252+elizandrosg@users.noreply.github.com>
2023-02-08 19:48:36 -0300

    Add files via upload

---
+++ aula006.py
@@ -0,0 +1,15 @@
+# Iniciar com letra, pode conter numeros, separar _, letras minusculas
+
+nome='Dinho'
+print(nome, type(nome))
+
+nome = 'Dinho'
+idade = 41
+altura = 1.70
+e_maior = idade > 18
+data_1 = True
+data_atual = 2023
+peso = 113.0
+imc = (peso/(altura*altura))
+
+print(f'{nome} tem {idade} anos de idade e seu IMC é, {imc:.2f}')
