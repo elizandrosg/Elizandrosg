cef54c53fb26c86588b800b2425f6fa50747b349
Elizandro Gonçalves <55092252+elizandrosg@users.noreply.github.com>
2023-02-08 19:49:32 -0300

    Add files via upload

---
+++ aula008.py
@@ -0,0 +1,12 @@
+# Entrada de dados
+
+#nome = input('qual o seu nome? ')
+#print(f'O usuario digitou {nome} e o tipo da variavel é ', type(nome))
+
+#nome = input('digite seu nome. ')
+#idade = input('digite sua idade. ')
+#print(nome, 'tem,', idade, 'anos.')
+
+numero_1 = int(input('Digite um numero: '))
+numero_2 = int(input('Digite outro numero: '))
+print('Resultado é ', (numero_1 + numero_2))
